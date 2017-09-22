adf95ff3e8b35a1ea42a51c06a1fd4d078e732bb
philippe.roy <philippe.roy@carsales.com.au>
2017-09-22 13:33:06 +1000

    Compile build to es5

--- es/index.js
+++ es/index.js
@@ -1,23 +1,40 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
-import { createStore as reduxCreateStore } from 'redux';
-import ReducerRegistry from './reducerRegistry';
+exports.createStore = createStore;
 
-export function createStore(reducer, preloadedState, enhancer) {
+var _redux = require('redux');
+
+var _reducerRegistry = require('./reducerRegistry');
+
+var _reducerRegistry2 = _interopRequireDefault(_reducerRegistry);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function createStore(reducer, preloadedState, enhancer) {
 
     // Reducer must be an object
     //TODO: throw error if not an object
 
-    const reducerRegistry = new ReducerRegistry(reducer);
+    var reducerRegistry = new _reducerRegistry2.default(reducer);
 
-    const store = reduxCreateStore(reducerRegistry.getCombinedRegistry(), preloadedState, enhancer);
+    var store = (0, _redux.createStore)(reducerRegistry.getCombinedRegistry(), preloadedState, enhancer);
 
     return _extends({}, store, {
-        addReducer: (name, reducer) => {
+        addReducer: function addReducer(name, reducer) {
             reducerRegistry.add(name, reducer);
             return store.replaceReducer(reducerRegistry.getCombinedRegistry());
         },
-        getReducers: () => reducerRegistry.getRegistry(),
-        getCombinedReducers: () => reducerRegistry.getCombinedRegistry()
+        getReducers: function getReducers() {
+            return reducerRegistry.getRegistry();
+        },
+        getCombinedReducers: function getCombinedReducers() {
+            return reducerRegistry.getCombinedRegistry();
+        }
     });
 }
--- es/reducerRegistry.js
+++ es/reducerRegistry.js
@@ -1,21 +1,42 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
-import { combineReducers } from 'redux';
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-export default class ReducerRegistry {
-    constructor(rootReducer) {
+var _redux = require('redux');
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var ReducerRegistry = function () {
+    function ReducerRegistry(rootReducer) {
+        _classCallCheck(this, ReducerRegistry);
+
         this._registry = _extends({}, rootReducer);
     }
 
-    add(name, reducer) {
-        this._registry[name] = reducer;
-    }
+    _createClass(ReducerRegistry, [{
+        key: 'add',
+        value: function add(name, reducer) {
+            this._registry[name] = reducer;
+        }
+    }, {
+        key: 'getRegistry',
+        value: function getRegistry() {
+            return _extends({}, this._registry);
+        }
+    }, {
+        key: 'getCombinedRegistry',
+        value: function getCombinedRegistry() {
+            return (0, _redux.combineReducers)(_extends({}, this._registry));
+        }
+    }]);
 
-    getRegistry() {
-        return _extends({}, this._registry);
-    }
+    return ReducerRegistry;
+}();
 
-    getCombinedRegistry() {
-        return combineReducers(_extends({}, this._registry));
-    }
-}
+exports.default = ReducerRegistry;
